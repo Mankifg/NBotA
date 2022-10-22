6919132f609ac016d6d6a98e1b1a432650fa243b
Mankifg <83959255+Mankifg@users.noreply.github.com>
2022-10-22 19:47:19 +0200

    black formater

--- Cogs/profile.py
+++ Cogs/profile.py
@@ -1,4 +1,3 @@
-
 from turtle import heading
 import discord
 from discord.ext import commands
@@ -12,98 +11,90 @@ from functions import *
 base_link = "https://data.nba.net/10s/prod/v1/{}/players.json"
 pic_link = "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/{}.png"
 
+
 class playerCog(commands.Cog, name="player command"):
-	def __init__(self, bot:commands.bot):
-		self.bot = bot
-        
-	@commands.command(name = "profile",
-					usage=" [name/surname] or [id]",
-					description = "Display the static players profile.")
-	@commands.cooldown(1, 2, commands.BucketType.member)
-	async def profile(self, ctx, i1, i2=None):
-		
-		succes,pdata = getplayerbydata(i1,i2)
+    def __init__(self, bot: commands.bot):
+        self.bot = bot
 
-		if not succes:
-			await ctx.send(f"The search query for `{i1} {i2}` is not found.")
-			return
+    @commands.command(
+        name="profile",
+        usage=" [name/surname] or [id]",
+        description="Display the static players profile.",
+    )
+    @commands.cooldown(1, 2, commands.BucketType.member)
+    async def profile(self, ctx, i1, i2=None):
 
-		draft = pdata["draft"]
-		num = int(draft["pickNum"])	
+        succes, pdata = getplayerbydata(i1, i2)
 
-		await ctx.send(pdata)
+        if not succes:
+            await ctx.send(f"The search query for `{i1} {i2}` is not found.")
+            return
 
-		dateofbirth = pdata["dateOfBirthUTC"].split("-")
-		
-		
-		q = discord.Embed(title=f'{pdata["jersey"]} | {pdata["firstName"]} {pdata["lastName"]} | {getteamfromid(pdata["teamId"])}',
-			description=f"User's id: {pdata['personId']}, Team's id: {pdata['teamId']}",
-			color=discord.Color.blue()
-		)
+        draft = pdata["draft"]
+        num = int(draft["pickNum"])
 
-		q.set_image(url=pic_link.replace("{}", pdata['personId']))
+        dateofbirth = pdata["dateOfBirthUTC"].split("-")
 
-		q.add_field(
-			name="Height",
-			value=f'{pdata["heightMeters"]}m / {pdata["heightFeet"]}′{pdata["heightInches"]}″',
-			inline=True
-		)
+        q = discord.Embed(
+            title=f'{pdata["jersey"]} | {pdata["firstName"]} {pdata["lastName"]} | {getteamfromid(pdata["teamId"])}',
+            description=f"User's id: {pdata['personId']}, Team's id: {pdata['teamId']}",
+            color=discord.Color.blue(),
+        )
+
+        q.set_image(url=pic_link.replace("{}", pdata["personId"]))
+
+        q.add_field(
+            name="Height",
+            value=f'{pdata["heightMeters"]}m / {pdata["heightFeet"]}′{pdata["heightInches"]}″',
+            inline=True,
+        )
+
+        q.add_field(
+            name="Weight",
+            value=f'{pdata["weightKilograms"]}kg / {pdata["weightPounds"]}lb',
+            inline=True,
+        )
+
+        q.add_field(
+            name="Date Of Birth",
+            value=f"{dateofbirth[2]}. {dateofbirth[1]}. {dateofbirth[0]}",
+            inline=True,
+        )
+
+        q.add_field(name="Country", value=f"{pdata['country']}", inline=True)
+
+        index = ""
+        for i in range(len(pdata["teams"])):
+            index = (
+                index
+                + f"{getteamfromid(pdata['teams'][i]['teamId'])} {pdata['teams'][i]['seasonStart']} - {pdata['teams'][i]['seasonEnd']}"
+                + "\n"
+            )
+
+        q.add_field(name="ALL Teams", value=index, inline=False)
+
+        if num == 1:
+            endpoint = "st"
+        elif num == 2:
+            endpoint = "nd"
+        elif num == 3:
+            endpoint = "rd"
+        else:
+            endpoint = "th"
+
+        q.add_field(
+            name="Draft Info",
+            value=f'Picked **{num}{endpoint}** in round **{draft["roundNum"]}**, by **{getteamfromid(draft["teamId"])}** year **{draft["seasonYear"]}**.',
+            inline=False,
+        )
+
+        q.add_field(
+            name="Other Data",
+            value=f"Playing in NBA for **{pdata['yearsPro']}** years, Citizenship: **{pdata['country']}**",
+            inline=False,
+        )
+        await ctx.send(embed=q)
 
 
-		q.add_field(
-			name="Weight",
-			value=f'{pdata["weightKilograms"]}kg / {pdata["weightPounds"]}lb',
-			inline=True
-		)	
-
-		q.add_field(
-			name="Date Of Birth",
-			value=f"{dateofbirth[2]}. {dateofbirth[1]}. {dateofbirth[0]}",
-			inline=True
-		)
-
-		q.add_field(
-			name="Country",
-			value=f"{pdata['country']}", inline=True
-		)
-
-		
-		index = ''
-		for i in range(len(pdata["teams"])):
-			index = index + f"{getteamfromid(pdata['teams'][i]['teamId'])} {pdata['teams'][i]['seasonStart']} - {pdata['teams'][i]['seasonEnd']}" + "\n"
-
-		
-	
-
-		q.add_field(
-			name="ALL Teams", value=index,inline=False
-		)
-
-		
-
-		if num == 1:
-			endpoint = "st"
-		elif num == 2:
-			endpoint = "nd"
-		elif num == 3:
-			endpoint = "rd"
-		else:
-			endpoint = "th"
-
-		q.add_field(
-			name="Draft Info", 
-			value=f'Picked **{num}{endpoint}** in round **{draft["roundNum"]}**, by **{getteamfromid(draft["teamId"])}** year **{draft["seasonYear"]}**.',
-			inline=False
-		)
-
-		q.add_field(
-			name="Other Data",
-			value=f"Playing in NBA for **{pdata['yearsPro']}** years, Citizenship: **{pdata['country']}**",
-			inline=False
-		)
-		await ctx.send(embed=q)
-			
-		
-
-def setup(bot:commands.Bot):
-	bot.add_cog(playerCog(bot))
+def setup(bot: commands.Bot):
+    bot.add_cog(playerCog(bot))
--- Cogs/stats.py
+++ Cogs/stats.py
@@ -1,57 +1,79 @@
 import discord
 from discord.ext import commands
 import requests, json
-from functions import * 
+from functions import *
 import math
-
-output = [
-	"",
-	"",
-	""
-]
-
 
 
 class statsCog(commands.Cog, name="stats command"):
-	def __init__(self, bot:commands.bot):
-		self.bot = bot
-        
-	@commands.command(name = "stats",
-					usage="",
-					description = "wip")
-	@commands.cooldown(1, 2, commands.BucketType.member)
-	async def stats(self, ctx, i1, i2=None, mode=""):
-		
-		succes,pdata = getplayerbydata(i1,i2)
-		if not succes:
-			await ctx.send(f"The search query for `{i1} {i2}` is not found.")
-			return	
+    def __init__(self, bot: commands.bot):
+        self.bot = bot
+
+    @commands.command(name="stats", usage="", description="wip")
+    @commands.cooldown(1, 2, commands.BucketType.member)
+    async def stats(self, ctx, i1, i2=None, mode=""):
+
+        succes, pdata = getplayerbydata(i1, i2)
+        if not succes:
+            await ctx.send(f"The search query for `{i1} {i2}` is not found.")
+            return
+
+        profileData = getprofilepersondata(pdata["personId"])
+        succes, wantdata, mode = frommodetodata(profileData, mode)
+
+        if not succes:
+            q = discord.Embed(
+                title="Error",
+                description=f"{wantdata}\nPlease do: [prefix]help stats",
+                color=discord.Color.red(),
+            )
+            await ctx.send(embed=q)
+            return
+
+        wantedlist = getwantedlist(wantdata)
+
+        q = discord.Embed(
+            title=f'{pdata["jersey"]} | {pdata["firstName"]} {pdata["lastName"]} | {getteamfromid(pdata["teamId"])}',  # ? add team
+            description=f"User's id: {pdata['personId']}, Team's id: {pdata['teamId']}",
+            color=discord.Color.blue(),
+        )
+
+        output = [
+            f"Points: {wantdata['points']}",
+            f"Points per game {wantdata['ppg']}",
+            f"Rebounds: {wantdata['totReb']}",
+            f"Rebounds per game: {wantdata['rpg']}",
+            f"Offensive rebounds: {wantdata['offReb']}",
+            f"Defensive rebounds: {wantdata['defReb']}",
+            f"Assists: {wantdata['assists']}",
+            f"Assists per game: {wantdata['apg']}",
+            f"Turnovers per game: {wantdata['topg']}",
+            f"Time played: {wantdata['mpg']} m",
+            f"Steals: {wantdata['steals']}" f"Steals per game: {wantdata['spg']}",
+            f"Blocks: {wantdata['blocks']}" f"Blocks per game: {wantdata['bpg']}",
+            f"Free trowns: {wantdata['ftp']}%",
+            f"Field goal percentage: {wantdata['fgp']}%",
+            f"Three point percentage {wantdata['tpp']}%",
+            f"Lost balls: {wantdata['turnovers']}",
+            f"Field goals made: {wantdata['fgm']}",
+            f"Field goal attempts: {wantdata['fga']}",
+            f"3-Points made {wantdata['tpm']}"
+            f"3-Points attempts: {wantdata['tpa']}"
+            f"Free throws made: {wantdata['ftm']}",
+            f"Free Throw Attempt: {wantdata['fta']}",
+            f"Player fouls {wantdata['pFouls']}",
+            f"Games played: {wantdata['gamesPlayed']}, started: {wantdata['gamesStarted']}",
+            f"Double double: {wantdata['dd2']}",
+            f"Tripple double: {wantdata['td3']}",
+        ]
+
+        out = ""
+        for item in output:
+            out = out + item + "\n"
+
+        q.add_field(name="Data", value=out)
+        await ctx.send(embed=q)
 
 
-		profileData = getprofilepersondata(pdata["personId"])
-		succes, wantdata, mode = frommodetodata(profileData,mode)
-
-
-		if not succes:
-			q = discord.Embed(
-				title="Error",
-				value=f"{wantdata}\nPlease do: [prefix]help stats",
-				color=discord.Color.red()
-			)
-			await ctx.send(embed=q)
-			return 
-
-		await ctx.send(wantdata)
-
-		#await ctx.send(profileData)
-
-		q = discord.Embed(title=f'{pdata["jersey"]} | {pdata["firstName"]} {pdata["lastName"]} | {getteamfromid(pdata["teamId"])}', #? add team
-			description=f"User's id: {pdata['personId']}, Team's id: {pdata['teamId']}",
-			color=discord.Color.blue()
-		)
-
-
-	
-
-def setup(bot:commands.Bot):
-	bot.add_cog(statsCog(bot))
+def setup(bot: commands.Bot):
+    bot.add_cog(statsCog(bot))
